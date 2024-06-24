8c3f2217ad1324c957555c4760a3afbd7862a750
Jack Westbrook <jack.westbrook@gmail.com>
2024-06-24 18:17:29 +0200

    wire it all up and remove testing export

--- packages/scenes-ml/src/index.ts
+++ packages/scenes-ml/src/index.ts
@@ -5,6 +5,3 @@ init().then(() => console.debug('Grafana ML initialized'));
 export { SceneBaseliner } from './components/SceneBaseliner';
 export { SceneChangepointDetector } from './components/SceneChangepointDetector';
 export { SceneOutlierDetector } from './components/SceneOutlierDetector';
-
-// TODO: make 'testing' a named export so that it can be imported as `import { testing } from '@grafana/scenes-ml/testing';`
-export * from './testing';
