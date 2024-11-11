cd21abaf9945166ba1e2aa88853253c2acc2bf87
Ben Sully <ben.sully88@gmail.com>
2024-11-11 17:16:46 +0000

    feat: add SceneTimeSeriesClusterer component
    This lets users run time series clustering and shows the clusters
    on the panel.

--- packages/scenes-ml/src/index.ts
+++ packages/scenes-ml/src/index.ts
@@ -1,6 +1,7 @@
 export { SceneBaseliner } from './components/SceneBaseliner';
 export { SceneChangepointDetector } from './components/SceneChangepointDetector';
 export { SceneOutlierDetector } from './components/SceneOutlierDetector';
+export { SceneTimeSeriesClusterer } from './components/SceneTimeSeriesClusterer';
 
 // TODO: make 'testing' a named export so that it can be imported as `import { testing } from '@grafana/scenes-ml/testing';`
 export * from './testing';
